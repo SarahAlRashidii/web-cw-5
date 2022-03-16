8b4a65197c50acc3ac50fe5b30299e5f15fe616a
Sarah AlRashidi <sarahnr1552004@gmail.com>
2022-03-16 19:36:47 +0300

    done
    day5 cw2

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -2,3 +2,10 @@ alert("Sarah")
 console.log(17);
 console.log('im in grade 12')
 
+let budget = 20;
+console.log(budget > 30 &&  budget < 80 );
+
+budget += 30;
+console.log(budget > 30 && budget < 80);
+
+
